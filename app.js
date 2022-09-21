ad0854168472c116d5f78e6a14eb23f2994702e9
Marc Charlebois <e2032252@cmaisonneuve.qc.ca>
2022-09-21 17:15:19 -0400

    Creation fichier pour la route de courses

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 const express = require('express');
-
 const app = express();
 const cors = require('cors');
-
-const personnes = require('./routes/personnes');
-const conditions = require('./routes/conditions');
-const connexion = require('./routes/connexion');
-const ippes = require('./routes/ippes');
-
 const PORT = process.env.PORT || 3000;
+
+const courses = require('./routes/courses');
+
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/personnes', personnes);
-app.use('/conditions', conditions);
-app.use('/connexion', connexion);
-app.use('/ippes', ippes);
+app.use('/courses', courses);
 
 app.listen(PORT, () => {
     console.log(`Mon application roule sur http://localhost:${PORT}`);
---
+++ routes/courses.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const request = require('../database/personnes');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    let data 
+})
