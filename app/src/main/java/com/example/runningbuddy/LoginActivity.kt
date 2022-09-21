6353495d5485f15946c29fa8160548f74c0e8de5
Antoine Lauzon <e1939748@cmaisonneuve.qc.ca>
2022-09-21 17:17:19 -0400

    Auto stash before merge of "main" and "origin/main"
    Action bar enlevé

---
+++ app/src/main/java/com/example/runningbuddy/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.runningbuddy
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+    }
+}
