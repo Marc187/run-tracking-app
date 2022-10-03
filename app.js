6d6d5a19c88e104a1e7e480ff1af14ceed98b6ea
Marc Charlebois <e2032252@cmaisonneuve.qc.ca>
2022-10-03 19:58:12 -0400

    Fonctionnement de la route de documentation

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./documentation_api.yaml')
 const PORT = process.env.PORT || 3000;
 
+// Packages nécessaires pour la documentation Swagger
+const YAML = require('yamljs')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = YAML.load('./documentation_api.yaml')
+
+
+// Importation des fichiers de routes
 const courses = require('./routes/courses');
 
 
+// Middlewares nécessaires à l'application
 app.use(cors());
 app.use(express.json());
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
+// Routes
 app.use('/courses', courses);
+
 
 app.listen(PORT, () => {
     console.log(`Mon application roule sur http://localhost:${PORT}`);
