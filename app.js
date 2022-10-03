7d3f9f07bc90c09cce7f6648a92b17d6deded919
Marc Charlebois <e2032252@cmaisonneuve.qc.ca>
2022-10-03 19:47:29 -0400

    Ajout de la route pour la documentation de l'api

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./documentation_api.yaml')
 const PORT = process.env.PORT || 3000;
 
 const courses = require('./routes/courses');
@@ -8,11 +10,9 @@ const courses = require('./routes/courses');
 
 app.use(cors());
 app.use(express.json());
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use('/courses', courses);
-app.get('/', (req, res) => {
-    res.send("<h1>Hello World! test heroku </h1>")
-})
 
 app.listen(PORT, () => {
     console.log(`Mon application roule sur http://localhost:${PORT}`);
