ddeb57711b33b8e9fcd729a0e500c5eb63a0f452
Marc Charlebois <e2032252@cmaisonneuve.qc.ca>
2022-10-03 21:59:29 -0400

    Update creation tables

--- database/creation_tables.sql
+++ database/creation_tables.sql
@@ -1,18 +1,18 @@
 USE [5D1Equipe04]
 GO 
 
-DROP TABLE IF EXISTS Likes
+DROP TABLE IF EXISTS likes
 GO
-DROP TABLE IF EXISTS Courses
+DROP TABLE IF EXISTS courses
 GO
-DROP TABLE IF EXISTS Abonnements
+DROP TABLE IF EXISTS abonnements
 GO
-DROP TABLE IF EXISTS Utilisateurs
+DROP TABLE IF EXISTS utilisateurs
 GO
 
 
 -- Creation des tables
-CREATE TABLE Utilisateurs (
+CREATE TABLE utilisateurs (
 	id INT PRIMARY KEY IDENTITY(1, 1),
 	nom_utilisateur VARCHAR(255),
 	nom VARCHAR(255),
@@ -21,24 +21,32 @@ CREATE TABLE Utilisateurs (
 )
 GO
 
-CREATE TABLE Abonnements (
-	id_utilisateur_suivi INT FOREIGN KEY REFERENCES Utilisateurs(id),
-	id_utilisateur_suivant INT FOREIGN KEY REFERENCES Utilisateurs(id)
+CREATE TABLE abonnements (
+	id_utilisateur_suivi INT 
+		FOREIGN KEY REFERENCES utilisateurs(id),
+	id_utilisateur_suivant INT 
+		FOREIGN KEY REFERENCES utilisateurs(id)
 )
 GO
 
-CREATE TABLE Courses (
+CREATE TABLE courses (
 	id INT PRIMARY KEY IDENTITY(1, 1),
-	id_utilisateur INT FOREIGN KEY REFERENCES Utilisateurs(id),
+	id_utilisateur INT 
+		FOREIGN KEY REFERENCES utilisateurs(id)
+		ON DELETE CASCADE,
 	distance FLOAT,
 	duree TIME,
 	date DATE
 )
 GO
 
-CREATE TABLE Likes (
-	id_course INT FOREIGN KEY REFERENCES Courses(id),
-	id_utilisateur INT FOREIGN KEY REFERENCES Utilisateurs(id)
+CREATE TABLE likes (
+	id_course INT 
+		FOREIGN KEY REFERENCES courses(id),
+		-- ON DELETE CASCADE,
+	id_utilisateur INT 
+		FOREIGN KEY REFERENCES utilisateurs(id),
+		-- ON DELETE CASCADE
 )
 GO
 
@@ -46,14 +54,14 @@ GO
 
 
 -- Insertion de donnees dans les tables
-INSERT INTO Utilisateurs VALUES 
+INSERT INTO utilisateurs VALUES 
 	('marc1234', 'Marc Charlebois', 'eric@gmail.com', 'password123'),
 	('eric1234', 'Eric Larivee', 'eric@gmail.com', 'password123'),
 	('antoine1234', 'Antoine Lauzon', 'antoine@gmail.com', 'password123'),
 	('test1234', 'Hello World', 'test@gmail.com', 'password123')
 GO
 
-INSERT INTO Abonnements VALUES 
+INSERT INTO abonnements VALUES 
 	(1, 2),
 	(1, 3),
 	(1, 4),
@@ -64,7 +72,7 @@ INSERT INTO Abonnements VALUES
 	(4, 2)
 GO
 
-INSERT INTO Courses VALUES
+INSERT INTO courses VALUES
 	(1, 5.54, '00:25:43', '2022-01-24'),
 	(1, 4.32, '00:15:43', '2022-02-25'),
 	(2, 5.54, '00:25:43', '2022-03-15'),
@@ -79,7 +87,7 @@ INSERT INTO Courses VALUES
 	(4, 2.01, '00:25:43', '2022-05-05')
 GO
 
-INSERT INTO Likes VALUES 
+INSERT INTO likes VALUES 
 	(1, 2),
 	(1, 3),
 	(2, 1),
