9a2faeab0086eeb2f9b1188ae8511032bc0909f9
Marc Charlebois <e2032252@cmaisonneuve.qc.ca>
2022-10-05 17:07:09 -0400

    update navbar/home

--- app/src/main/java/com/example/runningbuddy/MainActivity.kt
+++ app/src/main/java/com/example/runningbuddy/MainActivity.kt
@@ -1,13 +1,5 @@
 package com.example.runningbuddy
 
-import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.runningbuddy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,19 +8,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        this.setContentView(R.layout.activity_main)
 
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        this.setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val navView: BottomNavigationView = this.findViewById(R.id.nav_view)
         navView.setupWithNavController(navController)
     }
 }
