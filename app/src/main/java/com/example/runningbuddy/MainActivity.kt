540f89327b98d756118f6da9fdf72135698721d0
Guillaume Lachance <glachance@cmaisonneuve.qc.ca>
2022-10-05 20:40:45 -0400

    setupActionBarWithNavController fix

--- app/src/main/java/com/example/runningbuddy/MainActivity.kt
+++ app/src/main/java/com/example/runningbuddy/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.runningbuddy
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.runningbuddy.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
