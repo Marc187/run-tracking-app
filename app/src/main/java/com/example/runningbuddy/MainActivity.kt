be2828636c71abd53eb69688817d334b1726d17d
Marc Charlebois <e2032252@cmaisonneuve.qc.ca>
2022-10-11 11:54:49 -0400

    Refactorisation Navbar - Enregistrer une course

--- app/src/main/java/com/example/runningbuddy/MainActivity.kt
+++ app/src/main/java/com/example/runningbuddy/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home,
-                R.id.navigation_dashboard,
+                R.id.navigation_enregistrercourse,
                 R.id.navigation_notifications
             )
         )
