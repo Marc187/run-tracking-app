5248eaa6c19f789572706d52a1376f6c22afffb5
Marc Charlebois <e2032252@cmaisonneuve.qc.ca>
2022-11-02 17:02:20 -0400

    Route /activity retourne le nom de l'utilisateur

--- database/activity.js
+++ database/activity.js
@@ -4,6 +4,8 @@ const knex = knexModule(infoConnexion);
 
 function getActivity(liste_abonnements) {
     return knex('courses')
+        .select('courses.id', 'id_utilisateur', 'distance', 'duree', 'date', 'nom')
+        .join('utilisateurs', 'courses.id_utilisateur', 'utilisateurs.id')
         .whereIn('id_utilisateur', liste_abonnements)
         .orderBy('date')
 }
