d0fac76dd07bb00797b7f3dec37b88fd6a9a7883
Marc Charlebois <e2032252@cmaisonneuve.qc.ca>
2022-11-28 12:57:59 -0500

    Auto stash before merge of "main" and "origin/main"

--- app/src/main/java/com/example/runningbuddy/ui/enregistrercourse/EnregistrerCourseViewModel.kt
+++ app/src/main/java/com/example/runningbuddy/ui/enregistrercourse/EnregistrerCourseViewModel.kt
@@ -1,15 +1,18 @@
 package com.example.runningbuddy.ui.enregistrercourse
 
 import android.app.Application
+import android.graphics.Bitmap
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import com.example.runningbuddy.models.RunPost
 import com.example.runningbuddy.repositories.RunRepository
 
 class EnregistrerCourseViewModel(val app: Application) : AndroidViewModel(app) {
-
+    var id_course = MutableLiveData<Int>()
     private var runRepository: RunRepository = RunRepository(app)
 
-    fun insertRun(runPost: RunPost) {
-        runRepository.insertRun(runPost)
+
+    fun insertRun(runPost: RunPost, imageBitmap: Bitmap) {
+        runRepository.insertRun(runPost, id_course, imageBitmap)
     }
 }
