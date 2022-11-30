ac7428c69a083c9b51e713982485c0e2bf44ccaf
Eric <e2050477@cmaisonneuve.qc.ca>
2022-11-30 16:07:03 -0500

    fix des routes user pour friends

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const request = require('../database/utilisateurs')
+const auth = require('../middleware/authentification')
+const userVerification = require('../middleware/user_verification')
+const usersRequest = require('../database/utilisateurs')
+const abonnementsRequest = require('../database/abonnements')
 
-router.get('/:name', async (req, res) => {
+router.get('/:id_user/:name', auth, userVerification, async (req, res) => {
     try {
         // Accede a la BD et retourne les utilisateurs selon le nom
         const name = req.params.name
+        const id_user = req.params.id_user
         const users = await request.getUserByName(name)
+
+        // Verifie si le user a deja ete abonne
+        for (i = 0; i < users.length; i++) {
+            const subscribed = await abonnementsRequest.getSubscription(users[i].id, id_utilisateur)
+            users[i].subscribed = subscribed.length > 0 ? true : false
+        }
 
         res.status(200).json(users)
     } catch (error) {
