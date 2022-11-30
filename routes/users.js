bdce663168b827c032c0e2fe07ea918d53047a80
Eric <e2050477@cmaisonneuve.qc.ca>
2022-11-30 16:16:45 -0500

    update users

--- routes/users.js
+++ routes/users.js
@@ -5,16 +5,16 @@ const userVerification = require('../middleware/user_verification')
 const usersRequest = require('../database/utilisateurs')
 const abonnementsRequest = require('../database/abonnements')
 
-router.get('/:id_user/:name', auth, userVerification, async (req, res) => {
+router.get('/:id_user/:name', auth, async (req, res) => {
     try {
         // Accede a la BD et retourne les utilisateurs selon le nom
         const name = req.params.name
         const id_user = req.params.id_user
-        const users = await request.getUserByName(name)
+        const users = await usersRequest.getUserByName(name)
 
         // Verifie si le user a deja ete abonne
         for (i = 0; i < users.length; i++) {
-            const subscribed = await abonnementsRequest.getSubscription(users[i].id, id_utilisateur)
+            const subscribed = await abonnementsRequest.getSubscription(users[i].id, id_user)
             users[i].subscribed = subscribed.length > 0 ? true : false
         }
 
